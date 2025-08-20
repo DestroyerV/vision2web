405d72a32ae9302d24f05d65cce7a9e880986bd6
DestroyerV <mishravaibhav12321@gmail.com>
2025-08-21 04:49:59 +0530

    add ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button onClick={() => invoke.mutate({ text: "hello world" })}>
+      <Input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <Button onClick={() => invoke.mutate({ value: inputValue })}>
         Invoke Background Job
       </Button>
     </div>
