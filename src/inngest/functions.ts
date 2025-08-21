dc8beca2a52101640446a53f84aa9ea8e9c5b034
DestroyerV <mishravaibhav12321@gmail.com>
2025-08-21 23:55:35 +0530

    add e2b sandbox template and integrate code interpreter functionality

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
 import { createAgent, gemini } from "@inngest/agent-kit";
+import { Sandbox } from "e2b";
+import { getSandbox } from "./util";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vision2web");
+      return sandbox.sandboxId;
+    });
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
       name: "code-agent",
@@ -22,10 +28,18 @@ export const helloWorld = inngest.createFunction(
     const prompt =
       `Write the following snippets. Reply with code blocks only:\n\n` +
       value.slice(0, 4000); // keep within token/cost bounds
+
     const { output } = await codeAgent.run(prompt);
 
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
     return {
-      summary: output,
+      output,
+      sandboxUrl,
     };
   }
 );
